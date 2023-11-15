a6d39a84a9bdc82e48d3d6e2c60c55913c7c14f6
VitorSB <vitor.s.barbosa@kroton.com.br>
2023-11-15 18:32:48 -0300

    [feat] Create project and first migration

---
+++ logi-data-transform/src/main/kotlin/vitorsb/project/logidatatransform/LogiDataTransformApplication.kt
@@ -0,0 +1,11 @@
+package vitorsb.project.logidatatransform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogiDataTransformApplication
+
+fun main(args: Array<String>) {
+	runApplication<LogiDataTransformApplication>(*args)
+}
---
+++ logi-data-transform/src/test/kotlin/vitorsb/project/logidatatransform/LogiDataTransformApplicationTests.kt
@@ -0,0 +1,13 @@
+package vitorsb.project.logidatatransform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LogiDataTransformApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
