2f6d276071279b61af2fb3d352596b0a90b12388
VitorSB <vitor.s.barbosa@kroton.com.br>
2023-11-23 01:49:05 -0300

    [fix] change function annotation in OrderController

--- logi-data-process/src/main/kotlin/vitorsb/project/logidataprocess/controller/OrderController.kt
+++ logi-data-process/src/main/kotlin/vitorsb/project/logidataprocess/controller/OrderController.kt
@@ -45,8 +45,8 @@ class OrderController {
     @ResponseBody
     @PostMapping("/processTxtFile")
     fun processTxtFile(
-        @RequestParam("file") file: MultipartFile,
-        @RequestParam("toSave", required = false) toSave: Boolean? = false
+        @PathVariable(name = "file") file: MultipartFile,
+        @PathVariable(name = "toSave", required = false) toSave: Boolean? = false
     ): ResponseEntity<MutableList<UserTxtFileResponseDTO>> {
         if (!file.contentType.equals("text/plain"))
             throw InvalidFileTypeException("M=processTxtFile - File The file must be text type")
